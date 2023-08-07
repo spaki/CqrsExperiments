3e63e51130987a6104938e0714c213ded92c3d81
Eduardo Spaki <eduardo@code21.com.br>
2023-08-07 16:18:47 -0300

    Addmin domain tests

---
+++ CqrsExperiments.Domain.Tests/Models/OrderTests.cs
@@ -0,0 +1,16 @@
+﻿using CqrsExperiments.Domain.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CqrsExperiments.Domain.Tests.Models
+{
+    [TestClass]
+    public class OrderTests
+    {
+        [TestMethod]
+        public void ShouldGenerateOrderNumber()
+        {
+            var entity = new Order("mail@provider.com", "Delivery Address 007", 10);
+            Assert.IsNotNull(entity.Number, $"Should Generate Order Number");
+        }
+    }
+}
---
+++ CqrsExperiments.Domain.Tests/Services/OrderServiceTests.cs
@@ -0,0 +1,30 @@
+﻿using CqrsExperiments.Domain.Interfaces;
+using CqrsExperiments.Domain.Models;
+using CqrsExperiments.Domain.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace CqrsExperiments.Domain.Tests.Services
+{
+    [TestClass]
+    public class OrderServiceTests
+    {
+        private OrderService orderService;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var orderDbRepositoryMock = new Mock<IOrderDbRepository>();
+            orderService = new OrderService(orderDbRepositoryMock.Object);
+        }
+
+        [TestMethod]
+        public async Task ShouldReturnSavedOrder()
+        {
+            var entity = new Order("mail@provider.com", "Delivery Address 007", 10);
+            orderService.Save(entity);
+
+            Assert.IsNotNull(entity.Number, $"Should Return Saved Order");
+        }
+    }
+}
